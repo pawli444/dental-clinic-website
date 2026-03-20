807dbaaac4e92aea63fc7b848aa516c73c469071
pawel <pawli444@gmail.com>
2026-03-20 17:00:59 +0100

    feat: add Navbar component with styling and logo

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,16 +1,23 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
 
+import logo from "../../../assets/dentist-logo.png"
 
-
-
+const clinicName = import.meta.env.VITE_CLINIC_NAME
 
 function Navbar(){
-
     return(
-        <>
-        aaa
-        </>
+        <nav className="nav-wrapper">
+            <NavLink to="/">
+               <img src={logo} alt={clinicName} />
+            </NavLink>
+            <ul>
+                <li role="button"><NavLink to="/" alt="logo">Home</NavLink></li>
+                <li role="button"><NavLink to="/uslugi">Usługi</NavLink></li>
+                <li role="button"><NavLink to="/kontakt">Kontakt</NavLink></li>
+                <li role="button"><NavLink to="/kontakt" className="btn-primary">Umów wizytę</NavLink></li>
+            </ul>
+        </nav>
     )
 }
-
-
 export default Navbar;
